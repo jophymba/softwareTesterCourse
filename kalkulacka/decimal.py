abfc596f9f44e8f939893d9ee98a41365c5826d4
jophymba <jophy.mba@icloud.com>
2025-11-15 14:22:34 +0700

    adding projects

---
+++ kalkulacka/decimal.py
@@ -0,0 +1,6 @@
+from decimal import Decimal
+
+a = Decimal(input())
+b = Decimal(input())
+rozdil = a - b
+print(f"Rozdil: {rozdil}")
---
+++ kalkulacka/round.py
@@ -0,0 +1,4 @@
+a = float(input())
+b = float(input())
+rozdil = round (a - b, 2)
+print(f"Rozdil: {rozdil}")
