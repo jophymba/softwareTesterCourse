e0a870e336ca6f51e512349963ba46e3490229c9
jophymba <jophy.mba@icloud.com>
2025-11-17 12:55:31 +0700

    new folder word_count

---
+++ test_lekce7/word_count.py
@@ -0,0 +1,15 @@
+print("Zadejte delší slovo: ")
+delsi_slovo = input()
+
+print("Zadejte kratší slovo: ")
+
+# Zde dokonči úlohu svým kódem...
+
+kratsi_slovo = input()
+
+a = (len(delsi_slovo))
+b = (len(kratsi_slovo))
+
+c = a - b
+
+print("Slova se liší délkou o", c, "znaků")
