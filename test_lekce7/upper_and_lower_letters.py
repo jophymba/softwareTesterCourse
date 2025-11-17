a51dc96b2ff858eaa2709e48736f1209cc4c4abb
jophymba <jophy.mba@icloud.com>
2025-11-17 12:36:14 +0700

    new folder

---
+++ test_lekce7/upper_and_lower_letters.py
@@ -0,0 +1,12 @@
+print("Zadej jméno: ")
+jmeno = input()
+
+print("Zadej příjmení: ")
+prijmeni = input()
+
+print("Zadej svůj věk: ")
+vek = input()
+vek = int(vek)
+
+print(jmeno.upper() + " " + prijmeni.upper())
+print("Za rok ti bude", vek + 1, "let.")
