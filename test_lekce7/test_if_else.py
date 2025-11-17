360c95877f23236e74d9e67ce0ec6bd8add7b2bc
jophymba <jophy.mba@icloud.com>
2025-11-17 16:15:03 +0700

    added new test

---
+++ test_lekce7/test_if_else.py
@@ -0,0 +1,15 @@
+smajlik = input("Zadej smajlíka: \n")
+if (smajlik == ":-)" ):
+    print("Tvůj smajlík je veselý")
+
+elif (smajlik == ":-(" ):
+    print("Tvůj smajlík je smutný")
+
+elif (smajlik == ":-*" ):
+    print("Tvůj smajlík je zamilovaný") 
+
+elif (smajlik == ":-P" ):
+    print("Tvůj smajlík je s vyplazeným jazykem")
+
+else:
+    print("Tvůj smajlík je neznáný")    
---
+++ test_lekce7/test_len.py
@@ -0,0 +1,6 @@
+jmeno = str(input("Zadej své jméno:"))
+a = (len(jmeno))
+if (a >= 3) and (a <=10):
+    print("Normální jméno")
+else:
+    print("Máš moc krátké nebo moc dlouhé jméno!")    
