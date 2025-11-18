6ded7b6da02de086c07b0a6c45f5dd9e9e984bc8
jophymba <jophy.mba@icloud.com>
2025-11-18 17:26:54 +0700

    added new folder

---
+++ test_lekce3/test_1.py
@@ -0,0 +1,7 @@
+print("Ahoj, jak se jmenuješ?")
+name = input()
+# input is Bill Gates
+print("Jaký jsi?")
+# input is hustodémonsky bohatý
+vlastnost = input()
+print( name + " je " + vlastnost)
---
+++ test_lekce3/test_2.py
@@ -0,0 +1,5 @@
+print("Zadej číslo k umocnění: ")
+cislo = int(input())
+# input is number 64
+cislo = (cislo * 64)
+print("Výsledek:", cislo)
