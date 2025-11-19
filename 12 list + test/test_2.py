93642105aa792141ae8e921c0bff06ed7d7b0c06
jophymba <jophy.mba@icloud.com>
2025-11-19 20:59:05 +0700

    uprava testu lekce 12

--- 12 list + test/test_2.py
+++ 12 list + test/test_2.py
@@ -1,14 +1,14 @@
 zelenina = ["zelí", "okurka", "rajče", "paprika", "ředkev", "mrkev", "brokolice"]
 ovoce = ["jablko", "hruška", "pomeranč", "jahoda", "banán", "kiwi", "malina"]
-a = 0
-b = "ano"
-while b == "ano":
+pocet_pokusu = 0
+dalsi_pokus = "ano"
+while dalsi_pokus == "ano":
    zapis =input("Zadej název libovolného ovoce nebo zeleniny:")
    if zapis in zelenina:
     print("Zadal jsi zeleninu")
    elif zapis in ovoce:
     print ("Zadal jsi ovoce")
    else: print ("Tvoje slovo nemám v seznamu")
-   a +=1
-   b = input("Přeješ si zadat další slovo? (ano/ne) ")
-print("Zadal jsi", a, "slov")
+   pocet_pokusu +=1
+   dalsi_pokus = input("Přeješ si zadat další slovo? (ano/ne) ")
+print("Zadal jsi", pocet_pokusu, "slov")
