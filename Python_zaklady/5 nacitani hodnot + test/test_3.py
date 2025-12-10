538735b5427593b1119a5988c088a542fac2e5e6
jophymba <jophy.mba@icloud.com>
2025-12-10 12:19:04 +0700

    novy test

---
+++ Python_zaklady/5 nacitani hodnot + test/test_3.py
@@ -0,0 +1,9 @@
+print("Zadej poloměr kruhu (cm): ")
+
+# vstup uživatele je 12.1
+
+vstup = float(input())
+obvod = vstup * 3.14 * 2
+print(f"Obvod zadaného kruhu je: {obvod} cm")
+obsah = 3.14 * vstup  * vstup
+print(f"Jeho obsah je {obsah} cm^2")
